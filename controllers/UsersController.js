ad9022c8d6f0e94525a89cff66cc0c7d86e44cbf
HillaArts <hassenyehillary@gmail.com>
2024-08-06 12:09:44 +0000

    Inside server.js, create the Express server:
    it should listen on the port set by the environment variable PORT or by default 5000
    it should load all routes from the file routes/index.js
    Inside the folder routes, create a file index.js that contains all endpoints of our API:
    
    GET /status => AppController.getStatus
    GET /stats => AppController.getStats
    Inside the folder controllers, create a file AppController.js that contains the definition of the 2 endpoints:
    
    GET /status should return if Redis is alive and if the DB is alive too by using the 2 utils created previously: { "redis": true, "db": true } with a status code 200
    GET /stats should return the number of users and files in DB: { "users": 12, "files": 1231 } with a status code 200
    users collection must be used for counting all users
    files collection must be used for counting all files
    Terminal 1:
    
    bob@dylan:~$ npm run start-server
    Server running on port 5000
    ...
    Terminal 2:
    
    bob@dylan:~$ curl 0.0.0.0:5000/status ; echo ""
    {"redis":true,"db":true}
    bob@dylan:~$
    bob@dylan:~$ curl 0.0.0.0:5000/stats ; echo ""
    {"users":4,"files":30}
    bob@dylan:~$

---
+++ controllers/UsersController.js
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+    static async postNew(req, res) {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.status(400).json({ error: 'Missing email' });
+        }
+        if (!password) {
+            return res.status(400).json({ error: 'Missing password' });
+        }
+
+        const usersCollection = dbClient.db.collection('users');
+        const user = await usersCollection.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ error: 'Already exist' });
+        }
+
+        const hashedPassword = sha1(password);
+        const newUser = {
+            email,
+            password: hashedPassword,
+        };
+
+        await usersCollection.insertOne(newUser);
+        const newUserId = newUser._id;
+
+        return res.status(201).json({ id: newUserId, email });
+    }
+}
+
+export default UsersController;
